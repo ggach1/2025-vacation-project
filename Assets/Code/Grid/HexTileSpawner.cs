cd86d005af63c09da8a12f4785064d116611ff88
ggach16 <dlsdnj1234@gmail.com>
2025-07-15 23:39:30 +0900

    fixed : field % bench tile renderer

--- Assets/Code/Grid/HexTileSpawner.cs
+++ Assets/Code/Grid/HexTileSpawner.cs
@@ -9,36 +9,53 @@ namespace Code.Grid
     {
         [SerializeField] Transform originField;
 
-        [SerializeField] float radius = 0.5f;
+        [SerializeField] Vector2 fieldSize = new Vector2(8f, 6f);
         int _rows = 4;
         int _cols = 9;
 
         private void Start()
         {
-            float xStep = radius * Mathf.Sqrt(3f); // 가로
-            float zStep = radius * 1.5f; // 세로
+            BoxCollider collider = originField.GetComponent<BoxCollider>();
+            Vector3 fieldSize3D = Vector3.Scale(collider.size, originField.lossyScale);
+            Vector2 fieldSize = new Vector2(fieldSize3D.x, fieldSize3D.z);
 
-            for (int x = 0; x < _rows; x++)
+            float maxRadiusByWidth = (fieldSize.x / _cols) / Mathf.Sqrt(3f);
+            float maxRadiusByHeight = (fieldSize.y / _rows) / 1.5f;
+
+            float radius = Mathf.Min(maxRadiusByWidth, maxRadiusByHeight) * 0.95f;
+
+            float xStep = radius * Mathf.Sqrt(3f);
+            float zStep = radius * 1.5f;
+
+            float totalWidth = (_cols - 1) * xStep + xStep;
+            if (_rows > 1)
+                totalWidth += xStep / 2f; // 오른쪽 오프셋을 고려해서 해주어야 밖으로 안 삐져나감
+
+            float totalHeight = (_rows - 1) * zStep + zStep;
+
+            Vector3 originGridOffset = originField.position - new Vector3(totalWidth, 0, totalHeight) * 0.5f;
+
+            for (int row = 0; row < _rows; row++)
             {
-                for (int y = 0; y < _cols; y++)
+                for (int col = 0; col < _cols; col++)
                 {
-                    // 홀수 행은 x 방형으로 반 칸 이동
-                    float xOffset = (x % 2 == 0) ? 0f : xStep / 2f;
+                    float xOffset = (row % 2 == 0) ? 0f : xStep / 2f;
 
-                    float xPos = y * xStep + xOffset;
-                    float zPos = x * zStep;
+                    float xPos = col * xStep + xOffset;
+                    float zPos = row * zStep;
 
-                    Vector3 position = new Vector3(xPos, 0, zPos) + originField.position;
+                    Vector3 position = new Vector3(xPos, 0.01f, zPos) + originGridOffset;
 
-                    GameObject tile = new GameObject($"HexTile {x} {y}");
-                    tile.transform.position = position;
+                    GameObject tileObj = new GameObject($"HexTile {row} {col}");
+                    tileObj.transform.position = position;
+                    tileObj.transform.SetParent(originField);
 
-                    var renderer = tile.AddComponent<HexTileRenderer>();
+                    var renderer = tileObj.AddComponent<HexTileRenderer>();
                     renderer.Initialize(radius);
 
-                    Vector3Int cubeCoord = OffsetToCube(x, y);
+                    Vector3Int cubeCoord = OffsetToCube(col, row);
 
-                    var tiles = tile.AddComponent<HexTile>();
+                    var tiles = tileObj.AddComponent<HexTile>();
                     tiles.CubeCoord = cubeCoord;
                     tiles.IsWalkable = true;
 
